e091e75ac7a819929749826412c532628ed4f0d3
slawa loev <avenger_of_darkness1989@yahoo.de>
2022-10-07 14:32:46 +0200

    testing format

--- fast.py
+++ fast.py
@@ -16,26 +16,31 @@ import json
 app = Flask(__name__)
 
 # http://127.0.0.1:8000/predict?pickup_datetime=2012-10-06 12:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2
-#@app.post("/")
-#@app.get("/")
 
-@app.route("/")
+#@app.get("/") # fastapi interface
+@app.route("/") # flask interface
 def home():
     return {"Hello": "World"}
 
+#@app.post("/my_webhook")
 @app.route('/my_webhook', methods=['POST'])
 def handle_webhook(): # request
 
     req = request.get_json()
 
-    tag = req["fulfillmentInfo"]["tag"]
+    # tag = req["fulfillmentInfo"]["tag"]
 
-    if tag == "Default Welcome Intent":
-        text = "Hello from a GCF Webhook"
-    elif tag == "get-name":
-        text = "My name is Flowhook"
-    else:
-        text = f"There are no fulfillment responses defined for {tag} tag"
+
+    # parameters = []
+    # for key, value in req['sessionInfo']['parameters'].items():
+    #      parameters.append({'name':key,'value':value})
+
+    # if tag == "Default Welcome Intent":
+    #     text = "Hello from a GCF Webhook"
+    # elif tag == "get-name":
+    #     text = "My name is Flowhook"
+    # else:
+    #     text = f"There are no fulfillment responses defined for {tag} tag"
 
     # You can also use the google.cloud.dialogflowcx_v3.types.WebhookRequest protos instead of manually writing the json object
     # Please see https://googleapis.dev/python/dialogflow/latest/dialogflow_v2/types.html?highlight=webhookresponse#google.cloud.dialogflow_v2.types.WebhookResponse for an overview
@@ -45,7 +50,7 @@ def handle_webhook(): # request
                 {
                     "text": {
                         "text": [
-                            text
+                            req#text
                         ]
                     }
                 }
